2962b408803f3adc3782422af458f04008e37a04
Staso Stanislav <xctacx5@gmail.com>
2024-06-21 11:33:09 +0300

    Create UpdateProduct

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -15,19 +15,19 @@ public class CreateProductEndpoint : ICarterModule
     {
         app.MapPost("/products",
             async (CreateProductRequest request, ISender sender) =>
-        {
-            var command = request.Adapt<CreateProductCommand>();
+            {
+                var command = request.Adapt<CreateProductCommand>();
 
-            var result = await sender.Send(command);
+                var result = await sender.Send(command);
 
-            var response = result.Adapt<CreateProductResponse>();
+                var response = result.Adapt<CreateProductResponse>();
 
-            return Results.Created($"/product/{response.Id}", response);
-        })
-        .WithName("CreateProduct")
-        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
-        .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithSummary("Create Product")
-        .WithDescription("Create Product");
+                return Results.Created($"/product/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
     }
 }
--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -13,17 +13,17 @@ public class GetProductByCategoryEndpoint : ICarterModule
     {
         app.MapGet("/products/category/{category}",
             async (string category, ISender sender) =>
-        {
-            var result = await sender.Send(new GetProductByCategoryQuery(category));
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
 
-            var response = result.Adapt<GetProductByCategoryResponse>();
+                var response = result.Adapt<GetProductByCategoryResponse>();
 
-            return Results.Ok(response);
-        })
-        .WithName("GetProductByCategory")
-        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
-        .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithSummary("Get Product By Category")
-        .WithDescription("Get Product By Category");
+                return Results.Ok(response);
+            })
+            .WithName("GetProductByCategory")
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Category")
+            .WithDescription("Get Product By Category");
     }
 }
