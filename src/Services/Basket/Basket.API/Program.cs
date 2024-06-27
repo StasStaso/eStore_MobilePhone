0884b9d8f94bc77953697416f58cf26bbb976b6b
StasoStas <xctacx5@gmail.com>
2024-06-27 21:57:15 +0300

    HotFix

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
 // Add services to the container.
 builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LogginBehavior<,>));
+});
 
 var app = builder.Build();
 
+app.MapCarter();
 // Configure the HTTP request pipeline.
 app.Run();
