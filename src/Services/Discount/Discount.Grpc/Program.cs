60e88d7018591512158d95cefc8c0f2e288e5836
Staso Stanislav <xctacx5@gmail.com>
2024-07-02 23:18:23 +0300

    Update

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -16,7 +16,10 @@ builder.Services.AddDbContext<DiscountContext>(opts =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMigration();
+
 app.MapGrpcService<DiscountService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 if (app.Environment.IsDevelopment()) 
