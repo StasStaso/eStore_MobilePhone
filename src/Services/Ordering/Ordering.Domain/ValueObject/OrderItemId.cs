a88d84a3c55a7b65a287a82e825145b7ef096458
Staso Stanislav <xctacx5@gmail.com>
2024-07-10 19:47:08 +0300

    Update

---
+++ src/Services/Ordering/Ordering.Domain/ValueObject/OrderItemId.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Domain.ValueObject;
+
+public record OrderItemId
+{
+    public Guid Value { get; }
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObject/OrderName.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Domain.ValueObject;
+
+public record OrderName
+{
+    public string Value { get; }
+}
