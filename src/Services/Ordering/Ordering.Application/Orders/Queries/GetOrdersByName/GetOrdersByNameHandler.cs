63f33783d702c14447bc0178c7f4cfa8a3a47256
Staso Stanislav <xctacx5@gmail.com>
2024-08-02 10:24:05 +0300

    Add OrderCreateEventHandler Create GetOrderByNameQuery and CommandHandler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameHandler (IApplicationDbContext context)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await context.Orders
+            .Include(x => x.OrderItems)
+            .AsNoTracking()
+            .Where(x => x.OrderName.Value.Contains(query.Name))
+            .OrderBy(x => x.OrderName)
+            .ToListAsync(cancellationToken);
+
+        throw new NotImplementedException();
+    }
+}
