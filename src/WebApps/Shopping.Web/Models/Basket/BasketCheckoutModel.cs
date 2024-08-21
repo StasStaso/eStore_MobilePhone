8f943236fefe73a86ddde5156340fc167643acf3
Staso Stanislav <xctacx5@gmail.com>
2024-08-21 16:27:14 +0300

    Update

--- src/WebApps/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
+++ src/WebApps/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
@@ -16,8 +16,8 @@ public class BasketCheckoutModel
     public string ZipCode { get; set; } = default!;
 
     //Payment
-    public string CartName { get; set; } = default!;
-    public string CartNumber { get; set; } = default!;
+    public string CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
     public string Expiration { get; set; } = default!;
     public string CVV { get; set; } = default!;
     public int PaymentMethod { get; set; } = default!;
