a97099ce754a4bda620604be4615f54da3e34657
Staso Stanislav <xctacx5@gmail.com>
2024-08-21 18:02:34 +0300

    Update

--- src/WebApps/Shopping.Web/Pages/OrderList.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/OrderList.cshtml.cs
@@ -4,9 +4,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Shopping.Web.Pages;
 
 public class OrderListModel
+    (IOrderService orderService,
+    ILogger<OrderListModel> logger)
     : PageModel
 {
-    public void OnGet()
+    public IEnumerable<OrderModel> Orders { get; set; } = default!;
+    
+    public async Task<IActionResult> OnGetAsync()
     {
+        var customerId = new Guid("58c49479-ec65-4de2-86e7-033c546291aa");
+
+        var response = await orderService.GetOrdersByCustomer(customerId);
+        Orders = response.Orders;
+
+        return Page();
     }
 }
