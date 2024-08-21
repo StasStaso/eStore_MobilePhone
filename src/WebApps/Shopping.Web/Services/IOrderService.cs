943a17fbe849832652bcab74808e65c081b44e88
Staso Stanislav <xctacx5@gmail.com>
2024-08-21 18:16:57 +0300

    Update

--- src/WebApps/Shopping.Web/Services/IOrderService.cs
+++ src/WebApps/Shopping.Web/Services/IOrderService.cs
@@ -5,7 +5,7 @@ public interface IOrderService
     [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}\"")]
     Task<GetOrdersResponse> GetOrders(int? pageIndex = 1, int? pageSize = 10);
 
-    [Get("/ordering-service/orders/{orederName}")]
+    [Get("/ordering-service/orders/{orderName}")]
     Task<GetOrdersByNameResponse> GetOrdersByName(string orderName);
 
     [Get("/ordering-service/orders/customer/{customerId}")]
